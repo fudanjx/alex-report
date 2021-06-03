8339ce349bcf3ba72540b6e40fb02845348ed463
Jin Xin <fudanjx@gmail.com>
2021-06-03 17:41:02 +0800

    adding the function clean_raw_extraction_zip to read csv data directly from the zip

--- data_prep.py
+++ data_prep.py
@@ -41,6 +41,44 @@ def clean_raw_extraction(source_path, filename, wip_path, lookup_path):
     return df
 
 
+def clean_raw_extraction_zip(source_path, filename, wip_path, lookup_path):
+    # inspect the first 50 rows to find out which row start with actual data header.
+    # Assume all report will have case number field
+    # idx is the index row for the actual data starts, use this value to skip header when do 2nd import
+    df = pd.read_table(source_path + filename, sep='/t',
+                       skip_blank_lines=False, nrows=50,compression='zip', encoding="ISO-8859-1",
+                       engine='python')
+    col = df.columns
+    idx = df[df[col[0]].astype(str).str.contains("Case")].index.values
+
+    # read raw download file with 'skiprows' based on idx value obtained
+
+    df = pd.read_csv(source_path + filename, sep='\t',
+                     skiprows=idx[0] + 1, skipfooter=1, skip_blank_lines=True,compression='zip', encoding="ISO-8859-1",
+                     engine='python')
+    # find all the "unnamed" column which are empty column, and remove them
+    cols = [item for item in df.columns if item.lower()[:7] != 'unnamed']
+    df = df[cols]
+    # clean the header, in case there are trailing white space
+    headers = [header for header in df.columns]  # find header, using list comprehensions
+    headers = [header.replace('.', ' ') for header in headers]  # replace any header with . format
+    headers = [header.strip() for header in headers]  # Trim leading and trailing space
+    headers = [header.replace(' ', '_') for header in headers]
+    headers = [header.replace('__', '_') for header in headers]
+    headers = [header.replace('/', '_') for header in headers]
+    df.columns = headers  # replace header
+    # remove empty rows and duplicated header due to original paged data.
+    # Assume all reports will have 'Case No' field, use it to filter for AlEX cases (2800)
+    df = df.loc[df['Case_No'].notna(), :]
+    df = df.loc[df['Case_No'].str.contains('2800'), :]
+    df['cnt'] = 1
+    df = Date_Conversion(df)
+    mapping_Trt_Cat(df, lookup_path)
+    print(filename, " Processed")
+    # df.to_csv(wip_path + filename + '.csv', index=False)
+    return df
+
+
 def Date_Conversion(df_to_convert):
     headers = [header for header in df_to_convert.columns]
     if any('Adm_Date' in string for string in headers):
