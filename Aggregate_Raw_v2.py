f15f56bed80b9c9b5d8e710032f0ccea5bf48708
Jin Xin <fudanjx@gmail.com>
2021-06-03 17:51:58 +0800

    New aggregation script: 1. read only raw extraction from zip, 2. option to output both csv and parquet file, or only default parquet file, 3. export parquet with gzip compression

--- Aggregate_Raw_v2.py
+++ Aggregate_Raw_v2.py
@@ -4,109 +4,107 @@ import numpy as np
 import data_prep
 import data_prep as prep  # custom module for file cleaning
 import path as PT
+import locale
+locale.setlocale(locale.LC_TIME, "en_SG")  # singapore
 
 # to indicate whether want to save output as csv, along with the default parquet output.
 # no CSV output while equal to 0.
 require_csv = 0
-# import locale
-# locale.setlocale(locale.LC_TIME, "en_SG")  # singapore
-desired_width = 6200
-pd.set_option('display.width', desired_width)
-pd.set_option('display.max_columns', 12)
+
 # Step 1 -  combine inflight data
-# # # scan file log
-# filename_db = 'Combined_inflight.parquet.gzip'
-# filename_csv = 'Combined_inflight.csv'
-# filename_log = 'inflight_file_log.xlsx'
-# df_file_log = pd.read_excel(PT.path_wip_output_log + filename_log, sheet_name="file_log")
-# list_file_log = df_file_log['File'].tolist()
-#
-# # load the existing database, drop prelim data. if DB not found, reset to empty DB, and reset the filelist to empty
-# try:
-#     df_inflight = pd.read_parquet(PT.path_wip_output + filename_db)
-#     df_inflight = df_inflight.loc[df_inflight['prelim_flag'] != 'Y']
-#     df_inflight.drop_duplicates(inplace=True)
-# except:
-#     df_inflight = pd.DataFrame()
-#     list_file_log = []
-# # scan the files in the folder and compare with file log, to extract delta file list for extraction
-# list_all_files = [file for file in os.listdir(PT.path_inflight_data) if file.__contains__('.zip')]
-# files = [item for item in list_all_files if item not in list_file_log]
-# print("Inflight files to merge: ", files, end='\n')
-#
-# if len(files) != 0:
-#     # extraction for delta files only. Only update the filelist if prelim_flag is N/n
-#     for file in files:
-#         print(file, end='\n')
-#         prelim_flag = data_prep.prelim_flag_enter_validation()
-#         if prelim_flag.upper() != 'Y':
-#             list_file_log.append(file)
-#             # print(list_file_log,end='\n')
-#         current_data = prep.clean_raw_extraction_zip(PT.path_inflight_data, file, PT.path_wip_output, PT.path_lookup)
-#         current_data['prelim_flag'] = prelim_flag.upper()
-#         df_inflight = pd.concat([df_inflight, current_data])
-#
-#     # convert file list to df and write back to the log file
-#     df_file_log = pd.DataFrame(list_file_log, columns=['File'])
-#     df_file_log.to_excel(PT.path_wip_output_log + filename_log, sheet_name="file_log")
-#
-#     if require_csv != 0:
-#         df_inflight.to_csv(PT.path_wip_output + filename_csv, index=False)
-#     df_inflight.to_parquet(PT.path_wip_output + filename_db, compression='gzip', index=False)
-# else:
-#     pass
-# print("Inflight data: ", df_inflight.shape, 'Current data include below extraction files as final:')
-# print(df_file_log)
-# print("Step 1 successfully completed - Inflight data done")
-#
-# # Step 2 -  combine discharge data
 # # scan file log
-#
-# filename_db = 'Combined_disch.parquet.gzip'
-# filename_csv = 'Combined_disch.csv'
-# filename_log = 'disch_file_log.xlsx'
-# df_file_log = pd.read_excel(PT.path_wip_output_log + filename_log, sheet_name="file_log")
-# list_file_log = df_file_log['File'].tolist()
-# # load the existing database, drop prelim data. if DB not found, reset to empty DB, and reset the filelist to empty
-# try:
-#     df_dc = pd.read_parquet(PT.path_wip_output + filename_db)
-#     df_dc = df_dc.loc[df_dc['prelim_flag'] != 'Y']
-#     df_dc.drop_duplicates(inplace=True)
-# except:
-#     df_dc = pd.DataFrame()
-#     list_file_log = []
-# # scan the files in the folder and compare with file log, to extract delta file list for extraction
-# list_all_files = [file for file in os.listdir(PT.path_discharge_data) if file.__contains__('.zip')]
-# files = [item for item in list_all_files if item not in list_file_log]
-# print("Discharge data files to merge: ", files, end='\n')
-#
-# if len(files) != 0:
-#     # extraction for delta files only. Only update the filelist if prelim_flag is N/n
-#     for file in files:
-#         print(file, end='\n')
-#         prelim_flag = data_prep.prelim_flag_enter_validation()
-#         if prelim_flag.upper() != 'Y':
-#             list_file_log.append(file)
-#             # print(list_file_log)
-#         current_data = prep.clean_raw_extraction_zip(PT.path_discharge_data, file, PT.path_wip_output, PT.path_lookup)
-#         current_data['prelim_flag'] = prelim_flag.upper()
-#         df_dc = pd.concat([df_dc, current_data])
-#
-#     # convert file list to df and write back to the log file
-#     df_file_log = pd.DataFrame(list_file_log, columns=['File'])
-#     df_file_log.to_excel(PT.path_wip_output_log + filename_log, sheet_name="file_log")
-#
-#     # to avoid the encoding error
-#     df_dc['Postal'] = df_dc['Postal'].str.slice(0, 6)
-#     df_dc['Disch_Type'] = df_dc['Disch_Type'].str.slice(0, 2)
-#     if require_csv != 0:
-#         df_dc.to_csv(PT.path_wip_output + filename_csv, index=False)
-#     df_dc.to_parquet(PT.path_wip_output + filename_db, compression='gzip', index=False)
-# else:
-#     pass
-# print("Discharge data: ", df_dc.shape, 'Current data include below extraction files as final:')
-# print(df_file_log)
-# print("Step 2 successfully completed - discharge data done")
+filename_db = 'Combined_inflight.parquet.gzip'
+filename_csv = 'Combined_inflight.csv'
+filename_log = 'inflight_file_log.xlsx'
+df_file_log = pd.read_excel(PT.path_wip_output_log + filename_log, sheet_name="file_log")
+list_file_log = df_file_log['File'].tolist()
+
+# load the existing database, drop prelim data. if DB not found, reset to empty DB, and reset the filelist to empty
+try:
+    df_inflight = pd.read_parquet(PT.path_wip_output + filename_db)
+    df_inflight = df_inflight.loc[df_inflight['prelim_flag'] != 'Y']
+    df_inflight.drop_duplicates(inplace=True)
+except:
+    df_inflight = pd.DataFrame()
+    list_file_log = []
+# scan the files in the folder and compare with file log, to extract delta file list for extraction
+list_all_files = [file for file in os.listdir(PT.path_inflight_data) if file.__contains__('.zip')]
+files = [item for item in list_all_files if item not in list_file_log]
+print("Inflight files to merge: ", files, end='\n')
+
+if len(files) != 0:
+    # extraction for delta files only. Only update the filelist if prelim_flag is N/n
+    for file in files:
+        print(file, end='\n')
+        prelim_flag = data_prep.prelim_flag_enter_validation()
+        if prelim_flag.upper() != 'Y':
+            list_file_log.append(file)
+            # print(list_file_log,end='\n')
+        current_data = prep.clean_raw_extraction_zip(PT.path_inflight_data, file, PT.path_wip_output, PT.path_lookup)
+        current_data['prelim_flag'] = prelim_flag.upper()
+        df_inflight = pd.concat([df_inflight, current_data])
+
+    # convert file list to df and write back to the log file
+    df_file_log = pd.DataFrame(list_file_log, columns=['File'])
+    df_file_log.to_excel(PT.path_wip_output_log + filename_log, sheet_name="file_log")
+
+    if require_csv != 0:
+        df_inflight.to_csv(PT.path_wip_output + filename_csv, index=False)
+    df_inflight.to_parquet(PT.path_wip_output + filename_db, compression='gzip', index=False)
+else:
+    pass
+print("Inflight data: ", df_inflight.shape, 'Current data include below extraction files as final:')
+print(df_file_log)
+print("Step 1 successfully completed - Inflight data done")
+
+# Step 2 -  combine discharge data
+# scan file log
+
+filename_db = 'Combined_disch.parquet.gzip'
+filename_csv = 'Combined_disch.csv'
+filename_log = 'disch_file_log.xlsx'
+df_file_log = pd.read_excel(PT.path_wip_output_log + filename_log, sheet_name="file_log")
+list_file_log = df_file_log['File'].tolist()
+# load the existing database, drop prelim data. if DB not found, reset to empty DB, and reset the filelist to empty
+try:
+    df_dc = pd.read_parquet(PT.path_wip_output + filename_db)
+    df_dc = df_dc.loc[df_dc['prelim_flag'] != 'Y']
+    df_dc.drop_duplicates(inplace=True)
+except:
+    df_dc = pd.DataFrame()
+    list_file_log = []
+# scan the files in the folder and compare with file log, to extract delta file list for extraction
+list_all_files = [file for file in os.listdir(PT.path_discharge_data) if file.__contains__('.zip')]
+files = [item for item in list_all_files if item not in list_file_log]
+print("Discharge data files to merge: ", files, end='\n')
+
+if len(files) != 0:
+    # extraction for delta files only. Only update the filelist if prelim_flag is N/n
+    for file in files:
+        print(file, end='\n')
+        prelim_flag = data_prep.prelim_flag_enter_validation()
+        if prelim_flag.upper() != 'Y':
+            list_file_log.append(file)
+            # print(list_file_log)
+        current_data = prep.clean_raw_extraction_zip(PT.path_discharge_data, file, PT.path_wip_output, PT.path_lookup)
+        current_data['prelim_flag'] = prelim_flag.upper()
+        df_dc = pd.concat([df_dc, current_data])
+
+    # convert file list to df and write back to the log file
+    df_file_log = pd.DataFrame(list_file_log, columns=['File'])
+    df_file_log.to_excel(PT.path_wip_output_log + filename_log, sheet_name="file_log")
+
+    # to avoid the encoding error
+    df_dc['Postal'] = df_dc['Postal'].str.slice(0, 6)
+    df_dc['Disch_Type'] = df_dc['Disch_Type'].str.slice(0, 2)
+    if require_csv != 0:
+        df_dc.to_csv(PT.path_wip_output + filename_csv, index=False)
+    df_dc.to_parquet(PT.path_wip_output + filename_db, compression='gzip', index=False)
+else:
+    pass
+print("Discharge data: ", df_dc.shape, 'Current data include below extraction files as final:')
+print(df_file_log)
+print("Step 2 successfully completed - discharge data done")
 
 # Step 3 -  combine admission data
 # scan file log
